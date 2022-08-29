f25229966c623652495b9e332858ca405e5997ae
postforty <75767073+postforty@users.noreply.github.com>
2022-08-29 20:06:09 +0900

    [UPDATE] 로그인 구현 - 유효성 검사 - Model을 이용한 방식

--- ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
+++ ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -18,11 +18,14 @@ public class RegisterController {
 	
 //	@RequestMapping(value="/register/save", method=RequestMethod.POST)
 	@PostMapping("/register/save")
-	public String save(User user, Model model) throws Exception {
+	public String save(User user, Model m) throws Exception {
 		// 1. 유효성 검사
 		if(!isValid(user)) {
 			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.", "utf-8");
-			return "redirect:/register/add?msg="+msg; // URL재작성(rewriting)
+			
+			m.addAttribute("msg", msg);
+			return "redirect:/register/add";
+//			return "redirect:/register/add?msg="+msg; // URL재작성(rewriting)
 		}
 		
 		// 2. DB에 신규회원 정보를 저장
@@ -30,7 +33,7 @@ public class RegisterController {
 	}
 	
 	private boolean isValid(User user) {
-		return false;
+		return true;
 	}
 	
 	
