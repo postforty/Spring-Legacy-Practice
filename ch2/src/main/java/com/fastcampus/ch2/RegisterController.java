0dd3ebf33ec188218be47f49e771adac0bae6c59
postforty <75767073+postforty@users.noreply.github.com>
2022-08-29 20:00:00 +0900

    [ADD] 로그인 구현 - 유효성 검사

--- ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
+++ ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -1,17 +1,37 @@
 package com.fastcampus.ch2;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
-	@RequestMapping("/register/add")
-	public String register() {
-		return "registerForm";
-	}
+//	@RequestMapping(value="/register/save", method={RequestMethod.GET, RequestMethod.POST})
+//	@RequestMapping("/register/add")
+//	@GetMapping("/register/add")
+//	public String register() {
+//		return "registerForm";
+//	}
 	
-	@RequestMapping("/register/save")
-	public String save() {
+//	@RequestMapping(value="/register/save", method=RequestMethod.POST)
+	@PostMapping("/register/save")
+	public String save(User user, Model model) throws Exception {
+		// 1. 유효성 검사
+		if(!isValid(user)) {
+			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.", "utf-8");
+			return "redirect:/register/add?msg="+msg; // URL재작성(rewriting)
+		}
+		
+		// 2. DB에 신규회원 정보를 저장
 		return "registerInfo";
 	}
+	
+	private boolean isValid(User user) {
+		return false;
+	}
+	
+	
 }
